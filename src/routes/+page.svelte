82fb2f2dc7bb5c631883ea72a7496212f7db6f0c
titoco3000 <tito.guidotti@gmail.com>
2024-01-26 19:48:01 -0300

    simplifica tela inicial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { invoke } from '@tauri-apps/api/tauri'
-
-	let loadingEl: HTMLCanvasElement;
-
-	function drawArc() {
-		let context = loadingEl.getContext('2d');
-		if (context) {
-			let x = loadingEl.width / 2;
-			let y = loadingEl.height / 2;
-			let radius = 75;
-			let startAngle = 0.8 * Math.PI;
-			let endAngle = 1.9 * Math.PI;
-			let counterClockwise = false;
-
-			context.beginPath();
-			context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
-			context.lineWidth = 15;
-
-			// line color
-			context.strokeStyle = getComputedStyle(context.canvas).getPropertyValue('--cor-tema-forte');
-			context.stroke();
-		}
-	}
 	onMount(() => {
-		drawArc();
-
 		invoke("checar_tipo_de_janela").then(resultado=>{
 			if(resultado == "instalacao")
 				window.location.replace("instalacao");
@@ -36,40 +12,3 @@
 	});
 </script>
 
-<main>
-	<h1>Raja</h1>
-	<canvas bind:this={loadingEl} width="200" height="200"></canvas>
-</main>
-
-<style>
-	main {
-		height: 100vh;
-		width: 100vw;
-		margin: 0;
-		padding: 0;
-		background-color: var(--cor-tema-fundo-1);
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: space-around;
-	}
-	h1 {
-		width: 100%;
-		text-align: center;
-		font-size: 100px;
-		color: var(--cor-tema-fraca);
-		margin: 0;
-	}
-	@keyframes rotating {
-		from {
-			transform: rotate(0deg);
-		}
-		to {
-			transform: rotate(360deg);
-		}
-	}
-
-	canvas {
-		animation: rotating 0.8s linear infinite;
-	}
-</style>
